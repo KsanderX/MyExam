c2dc871d928d3952cfb6bfe06ec8fb5604fd8374
Александр Аганичев <sanya.kjh@mail.ru>
2026-04-20 13:54:56 +0700

    myExam

---
+++ demo/Windows/Main.xaml.cs
@@ -0,0 +1,212 @@
+﻿using demo.Data;
+using demo.Models;
+using demo.UserControllers;
+using demo.Windows.Products;
+using demo.Windows.RequestWin;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Reflection;
+using System.Transactions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace demo.Windows
+{
+    public partial class Main : Window
+    {
+        private DemoContext context;
+        private User currentUser;
+        private List<Product> products;
+        private readonly string projPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+        private string SortParam = "по возрастанию";
+        private string FiltParam = "все поставщики";
+
+        public Main()
+        {
+            context = new DemoContext();
+            InitializeComponent();
+            BoxUserName.Text = "гость";
+            PanelFind.Visibility = Visibility.Collapsed;
+            PanelBottomButton.Visibility = Visibility.Collapsed;
+            DrawProductItem(products);
+        }
+        public Main(User user)
+        {
+            context = new DemoContext();
+            InitializeComponent();
+
+            BoxUserName.Text = user.FullName;
+            currentUser = user;
+            
+            DrawProductItem(products);
+            DrawSuppliers();
+
+            if (user.RoleNavigation.Role1 == "Администратор")
+            {
+                BoxProduct.MouseDoubleClick += BoxProduct_MouseDoubleClick;
+            }
+            else
+            {
+                PanelBottomAdmin.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        public void DrawSuppliers()
+        {
+            List<Supplier> suppliers = new List<Supplier>()
+            {
+                new Supplier()
+                {
+                    Id = -1,
+                    Name = "все поставщики",
+                }
+            };
+            suppliers.AddRange(context.Suppliers.ToList());
+            ComboBoxItem.ItemsSource = suppliers;
+        }
+
+        private void DrawProductItem(List<Product> product)
+        {
+            if(BoxProduct != null)
+            {
+                BoxProduct.Items.Clear();
+                //BoxProduct.ItemsSource = product.Select(p => new ItemProduct(p));
+                foreach (var item in products)
+                {
+                    if (item != null)
+                    {
+                        ItemProduct xml = new ItemProduct(item);
+
+                        BoxProduct.Items.Add(xml);
+                    }
+                }
+            }
+        }
+
+        private void Button_exit_user(object sender, RoutedEventArgs e)
+        {
+            Authorization authorization = new Authorization();
+            authorization.Show();
+            this.Close();
+        }
+
+        private void BoxProduct_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ListBox list = sender as ListBox;
+            ItemProduct controller = list.SelectedItem as ItemProduct;
+            Product product = controller.DataContext as Product;
+            EditProduct edit = new EditProduct(product);
+
+            if (edit.ShowDialog() == true)
+            {
+                DrawProductItem(products);
+            }
+        }
+
+        private void Button_add_product(object sender, RoutedEventArgs e)
+        {
+            AddProduct add = new AddProduct();
+
+            if (add.ShowDialog() == true)
+            {
+                DrawProductItem(products);
+            }
+        }
+
+        private void Button_request(object sender, RoutedEventArgs e)
+        {
+            RequestWindows request = new RequestWindows(currentUser);
+            if (request.ShowDialog() == true)
+            {
+
+            }
+        }
+
+        //Поиск
+        private void BoxFind_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Sort();
+        }
+        //Сортировка
+        private void RadioUpp_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+
+            if (radio.Content.ToString() == "по возрастанию")
+            {
+                SortParam = "по возрастанию";
+            }
+            else if (radio.Content.ToString() == "по убыванию")
+            {
+                SortParam = "по убыванию";
+            }
+            Sort();
+        }
+        //Фильтрация
+        private void ComboSuppliers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox box = sender as ComboBox;
+            if (box.SelectedItem != null)
+            {
+                FiltParam = box.SelectedItem.ToString();
+            }
+            Sort();
+        }
+
+        public void Sort()
+        {
+            products = context.Products.Include(q => q.Supplier)
+                .Include(q => q.Manufacturer)
+                .Include(q => q.Name)
+                .Include(q => q.Category)
+                .ToList();
+
+            products = products.Where(q =>
+                (q.Description?.Contains(BoxFind.Text) ?? false)
+                || (q.Article?.Contains(BoxFind.Text) ?? false)
+                || (q.Name?.Name?.Contains(BoxFind.Text) ?? false)
+                || (q.Category?.Name?.Contains(BoxFind.Text) ?? false)
+                || (q.Manufacturer?.Name?.Contains(BoxFind.Text) ?? false)
+                || (q.Supplier.Name?.Contains(BoxFind.Text) ?? false)
+                ).Where(q => q.Supplier.Name == FiltParam
+                || FiltParam == "все поставщики").ToList();
+
+            if (SortParam == "по возрастанию")
+            {
+                products = products.OrderBy(q => q.Count).ToList();
+            }
+            else if (SortParam == "по убыванию")
+            {
+                products = products.OrderByDescending(q => q.Count).ToList();
+            }
+
+            DrawProductItem(products);
+        }
+
+        private void Buutton_delite_product(object sender, RoutedEventArgs e)
+        {
+            var selectedController = BoxProduct.SelectedItem as ItemProduct;
+
+            if (selectedController != null)
+            {
+                try
+                {
+                    Product selectedProduct = selectedController.DataContext as Product;
+                    context.Products.Remove(selectedProduct);
+                    context.SaveChanges();
+                    MessageBox.Show("Продукт успешно удален!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Sort();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Вы не можете удалить продукт который входит в состав заказа");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите продукт для удаления!", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ demo/Windows/Products/EditProduct.xaml.cs
@@ -0,0 +1,173 @@
+﻿using demo.Data;
+using demo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace demo.Windows.Products
+{
+    public partial class EditProduct : Window
+    {
+        private readonly string projPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        DemoContext context;
+        private Product product;
+        private BitmapImage selectImage;
+        private string? imageName = null;
+        public EditProduct(Product product)
+        {
+            InitializeComponent();
+
+            context = new DemoContext();
+            this.product = product;
+            Load();
+        }
+
+        private void Load()
+        {
+
+            if (product.ImagePath == null || product.ImagePath == "picture.png")
+            {
+                selectImage = new BitmapImage(new Uri(Path.Combine(projPath, "Images", "Defaults", "picture.png")));
+            }
+            else
+            {
+                selectImage = new BitmapImage(new Uri(Path.Combine(projPath, "Images",product.ImagePath)));
+            }
+
+            BoxImage.Source = selectImage;
+
+
+            BoxCategory.DisplayMemberPath = "Name";
+            BoxCategory.SelectedValuePath = "Id";
+            BoxCategory.ItemsSource = context.Categories.ToList();
+            BoxCategory.SelectedValue = product.CategoryId;
+
+            BoxSupplier.DisplayMemberPath = "Name";
+            BoxSupplier.SelectedValuePath = "Id";
+            BoxSupplier.ItemsSource = context.Suppliers.ToList();
+            BoxSupplier.SelectedValue = product.SupplierId;
+
+            BoxManufacturer.DisplayMemberPath = "Name";
+            BoxManufacturer.SelectedValuePath = "Id";
+            BoxManufacturer.ItemsSource = context.Manufacturers.ToList();
+            BoxManufacturer.SelectedValue = product.ManufacturerId;
+
+
+            BoxName.Text = product.Name.Name;
+            BoxDescription.Text = product.Description;
+            BoxDiscount.Text = product.Discount.ToString();
+            BoxPrice.Text = product.Price.ToString();
+            BoxUnit.Text = product.Unit.ToString();
+            BoxCount.Text = product.Count.ToString();
+        }
+
+        private void ButtonSaveProduct(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(BoxDescription.Text) ||
+                string.IsNullOrWhiteSpace(BoxDiscount.Text) ||
+                string.IsNullOrWhiteSpace(BoxCount.Text) ||
+                string.IsNullOrWhiteSpace(BoxPrice.Text) ||
+                string.IsNullOrWhiteSpace(BoxName.Text) ||
+                string.IsNullOrWhiteSpace(BoxUnit.Text))
+            {
+                MessageBox.Show("Заполните все поля");
+                return;
+            }
+            try
+            {
+
+                if (int.Parse(BoxDiscount.Text) < 0)
+                {
+                    MessageBox.Show("Скидка не может быть отрицательной");
+                    return;
+                }
+
+                if (int.Parse(BoxDiscount.Text) > 100)
+                {
+                    MessageBox.Show("Скидка не может быть больше 100 процентов");
+                    return;
+                }
+
+                if (int.Parse(BoxCount.Text) < 0)
+                {
+                    MessageBox.Show("Количество не может быть отрицательным значением");
+                    return;
+                }
+
+                if (int.Parse(BoxPrice.Text) < 0)
+                {
+                    MessageBox.Show("Цена не может быть отрицательной");
+                    return;
+                }
+
+                var name = context.ProductNames.FirstOrDefault(q => q.Name == BoxName.Text);
+                if (name == null)
+                {
+                    context.ProductNames.Add(new ProductName() { Id = context.ProductNames.Max(q => q.Id) + 1, Name = BoxName.Text });
+                    context.SaveChanges();
+                    name = context.ProductNames.FirstOrDefault(q => q.Name == BoxName.Text);
+                }
+
+                product.Name = name;
+                // product.Name = context.ProductNames.FirstOrDefault(q => q.Name == BoxName.Text) ?? new ProductName() { Id = context.ProductNames.Max(q => q.Id) + 1, Name = BoxName.Text };
+                product.Category = context.Categories.FirstOrDefault(q => q.Name == BoxCategory.SelectedItem.ToString());
+                product.Description = BoxDescription.Text;
+                product.Manufacturer = context.Manufacturers.FirstOrDefault(q => q.Name == BoxManufacturer.SelectedItem.ToString());
+                product.Supplier = context.Suppliers.FirstOrDefault(q => q.Name == BoxSupplier.SelectedItem.ToString());
+                product.Price = int.Parse(BoxPrice.Text);
+                product.Unit = BoxUnit.Text;
+                product.Count = int.Parse(BoxCount.Text);
+                product.Discount = int.Parse(BoxDiscount.Text);
+                if (imageName != null)
+                {
+                    product.ImagePath = imageName;
+                }
+
+                context.Entry(product).State = EntityState.Modified;
+                context.SaveChanges();
+
+                DialogResult = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"не верный формат ввода {ex.Message}");
+            }
+        }
+
+        private void ButtonExit(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void ButtonLoadImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            if (openFile.ShowDialog() == true)
+            {
+                Uri uri = new Uri(openFile.FileName);
+                BitmapImage select = new BitmapImage(uri);
+
+                string folderPath = Path.Combine(projPath, "Images");
+
+                string destFileName = Path.Combine(folderPath, openFile.SafeFileName);
+
+                try
+                {
+                    File.Copy(openFile.FileName, destFileName, true);
+
+                    selectImage = select;
+                    imageName = openFile.SafeFileName;
+                    BoxImage.Source = selectImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при копировании файла: {ex.Message}");
+                }
+            }
+        }
+    }
+}
